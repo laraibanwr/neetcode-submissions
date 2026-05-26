0094892df5e7f500b557defed8e7fe0ee2990755
Laraib Anwar <119056540+laraibanwr@users.noreply.github.com>
2026-05-26 22:37:11 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str: strs) {
+            int[] freq = new int[26];
+            for(char ch: str.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
